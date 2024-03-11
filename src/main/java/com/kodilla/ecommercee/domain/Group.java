9b67edd671cf71a43e4389fa170e6f471b6df61b
Daniel Nagorka <danielosnag1996@gmail.com>
2024-03-11 19:17:03 +0100

    Encja_Group_Implementacja

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,15 +1,35 @@
 package com.kodilla.ecommercee.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.util.List;
 
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Groups")
 public class Group {
+    @Id
+    @Column(name = "Group_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "NAME", unique = true)
+    @NonNull
     private String name;
+    @Column(name = "DESCRIPTION", unique = true)
+    @NonNull
     private String description;
+   @OneToMany(targetEntity = Product.class,
+   mappedBy = "group",
+   cascade = CascadeType.ALL,
+   fetch = FetchType.LAZY)
+    private List<Product> products;
 
 }
 
--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,16 +1,27 @@
 package com.kodilla.ecommercee.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-
+@Data
+@Entity
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+    @Id
     private int id;
     private String name;
     private String description;
     private BigDecimal price;
-    private int group_id;
+    @ManyToOne
+    @JoinColumn(name = "GROUP_ID")
+    private Group group;
 }
