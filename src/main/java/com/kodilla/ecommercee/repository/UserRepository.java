06dd93e2ac0fdad759b93766ee97d92ec9b793af
JakubWaw93 <149072115+JakubWaw93@users.noreply.github.com>
2024-03-15 14:29:45 +0100

    Update src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
    Co-authored-by: Zuzanna Jaworska <146731304+ZuzaJaworska@users.noreply.github.com>

--- src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
@@ -5,7 +5,6 @@ import com.kodilla.ecommercee.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     default void delete(User entity) {
