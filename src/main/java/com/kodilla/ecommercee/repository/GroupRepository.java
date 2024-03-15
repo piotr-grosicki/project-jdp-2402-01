36df46687b07305d6551339678e0f46685480864
JakubWaw93 <149072115+JakubWaw93@users.noreply.github.com>
2024-03-15 14:29:19 +0100

    Update src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
    Co-authored-by: Zuzanna Jaworska <146731304+ZuzaJaworska@users.noreply.github.com>

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -5,7 +5,6 @@ import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
 
     default void delete(Group entity) {
