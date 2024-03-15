0a20630259480fa5cbcec4033e950beba810ca04
JakubWaw93 <149072115+JakubWaw93@users.noreply.github.com>
2024-03-15 14:29:12 +0100

    Update src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
    Co-authored-by: Zuzanna Jaworska <146731304+ZuzaJaworska@users.noreply.github.com>

--- src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -5,7 +5,6 @@ import com.kodilla.ecommercee.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     default void delete(Cart entity) {
