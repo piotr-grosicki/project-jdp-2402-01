54197ee94d271f0fa63b01f1689e57e825767ee7
JakubWaw93 <149072115+JakubWaw93@users.noreply.github.com>
2024-03-15 14:29:33 +0100

    Update src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
    Co-authored-by: Zuzanna Jaworska <146731304+ZuzaJaworska@users.noreply.github.com>

--- src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -5,7 +5,6 @@ import com.kodilla.ecommercee.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     default void delete(Product entity) {
