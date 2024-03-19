045a7118a3ff0580c66e06490247f71da6a51c53
JakubWaw93 <149072115+JakubWaw93@users.noreply.github.com>
2024-03-19 15:19:30 +0100

    JDP-2402-01-22 v0.1 before postman tests

--- src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
@@ -2,12 +2,17 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GroupDto {
     private Long id;
     private String name;
     private String description;
+    private List<Long> productsIds;
     private boolean active;
 }
