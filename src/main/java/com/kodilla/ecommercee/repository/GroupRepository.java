9d2fd90766f56cfed3d45bcba51cc6aae516df59
JakubWaw93 <149072115+JakubWaw93@users.noreply.github.com>
2024-03-19 13:38:21 +0100

    JDP-2402-01-21 v0.3 added finding id of active group for product mapper

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -5,10 +5,17 @@ import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface GroupRepository extends JpaRepository<Group, Long> {
 
     default void delete(Group entity) {
         entity.setActive(false);
         save(entity);
     }
+
+    List<Group> findAllByActiveTrue();
+
+    Optional<Group> findByIdAndActiveTrue(Long id);
 }
