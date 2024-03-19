8425abc4655fc054bee67568f4bc7089019a3703
Zuzanna Jaworska <146731304+ZuzaJaworska@users.noreply.github.com>
2024-03-19 19:12:36 +0100

    JDP240201-18_Group_Entity_Tests (#24)
    * Group Entity Tests - first attempt
    
    * Group Entity Tests - bug fixed
    
    * Group Entity Tests - added testSaveProductToGroup
    
    * Group Entity Tests - check if products are active after delete(group)

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -1,11 +1,13 @@
 package com.kodilla.ecommercee.repository;
 
-import com.kodilla.ecommercee.domain.Cart;
 import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
+
+    List<Group> findAllByActiveTrue();
 
     default void delete(Group entity) {
         entity.setActive(false);
