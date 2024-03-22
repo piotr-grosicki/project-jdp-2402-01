a0e8288aa4e991235aafb61b4cbef1dfcc0f9073
JakubWaw93 <149072115+JakubWaw93@users.noreply.github.com>
2024-03-22 20:50:18 +0100

    Update GroupRepository.java

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -1,9 +1,7 @@
 package com.kodilla.ecommercee.repository;
 
-import com.kodilla.ecommercee.domain.Cart;
 import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
