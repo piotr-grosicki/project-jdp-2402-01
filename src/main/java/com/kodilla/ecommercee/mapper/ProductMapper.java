a2a9d72c2dfe564a914006c1cf193734faa42f1a
JakubWaw93 <149072115+JakubWaw93@users.noreply.github.com>
2024-03-22 15:08:45 +0100

    Update ProductMapper.java

--- src/main/java/com/kodilla/ecommercee/mapper/ProductMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/ProductMapper.java
@@ -5,8 +5,6 @@ import com.kodilla.ecommercee.domain.ProductDto;
 import com.kodilla.ecommercee.exception.GroupNotFoundException;
 import com.kodilla.ecommercee.repository.GroupRepository;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
