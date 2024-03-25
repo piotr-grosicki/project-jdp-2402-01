016a870f064d85bdd34d15b3363143e76805e8ca
Zuzanna Jaworska <zuzanna.ewa.lewandowska@gmail.com>
2024-03-25 20:30:09 +0100

    OrderController - GroupDto and GroupMapper adjusted to other mappers - works the same

--- src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
@@ -2,18 +2,15 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class GroupDto {
     private Long id;
     private String name;
     private String description;
-    private List<Long> productsIds = new ArrayList<>();
+    private List<Long> productsIds;
     private boolean active;
 }
